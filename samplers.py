dea3e267b80a0468d9b709b221b60d63d4ee01b4
ksolaiman <ksolaima@purdue.edu>
2021-02-11 20:12:10 -0500

    RandomIdentitySampler needed type as argument! may not work 100 percent yet

--- samplers.py
+++ samplers.py
@@ -18,7 +18,7 @@ class RandomIdentitySampler(Sampler):
         num_instances (int): number of instances per identity.
     """
     def __init__(self, data_source, num_instances=4):
-        super(RandomIdentitySampler).__init__()
+        super(RandomIdentitySampler).__init__(int) # added num_instances as argument
         self.data_source = data_source
         self.num_instances = num_instances
         self.index_dic = defaultdict(list)
